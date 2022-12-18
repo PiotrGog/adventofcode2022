322b99886c649f8eab9f3670a58fb37c65603ad3
piogog <gogola.piotr@interia.pl>
2022-12-18 09:32:27 +0100

    day 16 part 2??

--- day-16-proboscidea-volcanium/src/main.rs
+++ day-16-proboscidea-volcanium/src/main.rs
@@ -55,15 +55,15 @@ fn solve(
     let (flow_rate, next_valves) = data.get(current_valve).unwrap();
     let available_minutes_after_move = available_minutes - 1;
     if flow_rate > &0 && current_valve_is_not_turned_on {
-        let mut new_tuned_on = turned_on.clone();
-        new_tuned_on.insert(current_valve.clone());
+        let mut new_turned_on = turned_on.clone();
+        new_turned_on.insert(current_valve.clone());
 
         max_flow = max_flow.max(solve(
             data,
             current_valve,
             available_minutes_after_move,
             current_flow + (available_minutes_after_move as u32 * flow_rate),
-            new_tuned_on,
+            new_turned_on,
             conf,
         ));
     }
@@ -90,6 +90,124 @@ fn solve(
     max_flow
 }
 
+fn solve_with_elephant(
+    data: &HashMap<String, (u32, Vec<String>)>,
+    my_current_valve: &String,
+    elephant_current_valve: &String,
+    available_minutes: i32,
+    current_flow: u32,
+    turned_on: HashSet<String>,
+    conf: &mut HashMap<(String, String, i32, bool, bool), u32>,
+) -> u32 {
+    if available_minutes <= 0 {
+        return current_flow;
+    }
+
+    let my_current_valve_is_not_turned_on = !turned_on.contains(my_current_valve);
+    let elephant_current_valve_is_not_turned_on = !turned_on.contains(elephant_current_valve);
+    if let Some(v) = conf.get(&(
+        my_current_valve.to_string(),
+        elephant_current_valve.to_string(),
+        available_minutes,
+        my_current_valve_is_not_turned_on,
+        elephant_current_valve_is_not_turned_on,
+    )) {
+        if v >= &current_flow {
+            return *v;
+        }
+    }
+
+    let mut max_flow = u32::MIN;
+
+    let available_minutes_after_move = available_minutes - 1;
+    let next_steps = possible_next_steps(
+        data,
+        my_current_valve,
+        elephant_current_valve,
+        available_minutes_after_move,
+        turned_on,
+    );
+
+    for (my_conf, elephant_conf) in next_steps {
+        let mut turned_on = my_conf.2;
+        turned_on.extend(elephant_conf.2);
+        max_flow = max_flow.max(solve_with_elephant(
+            data,
+            &my_conf.0,
+            &elephant_conf.0,
+            available_minutes_after_move,
+            current_flow + my_conf.1 + elephant_conf.1,
+            turned_on,
+            conf,
+        ));
+    }
+
+    conf.insert(
+        (
+            my_current_valve.to_string(),
+            elephant_current_valve.to_string(),
+            available_minutes,
+            my_current_valve_is_not_turned_on,
+            elephant_current_valve_is_not_turned_on,
+        ),
+        current_flow,
+    );
+
+    max_flow
+}
+
+fn possible_next_steps(
+    data: &HashMap<String, (u32, Vec<String>)>,
+    my_current_valve: &String,
+    elephant_current_valve: &String,
+    available_minutes: i32,
+    turned_on: HashSet<String>,
+) -> Vec<(
+    (String, u32, HashSet<String>),
+    (String, u32, HashSet<String>),
+)> {
+    let (my_flow_rate, my_next_valves) = data.get(my_current_valve).unwrap();
+    let mut my_configurations = vec![];
+    let mut new_turned_on = turned_on.clone();
+    if my_flow_rate > &0 && !turned_on.contains(my_current_valve) {
+        new_turned_on.insert(my_current_valve.clone());
+        my_configurations.push((
+            my_current_valve.clone(),
+            available_minutes as u32 * my_flow_rate,
+            new_turned_on.clone(),
+        ));
+    }
+    for next_valve in my_next_valves {
+        my_configurations.push((next_valve.clone(), 0, turned_on.clone()));
+    }
+
+    let (elephant_flow_rate, elephant_next_valves) = data.get(elephant_current_valve).unwrap();
+    let mut elephant_configurations = vec![];
+    if elephant_flow_rate > &0
+        && my_current_valve != elephant_current_valve
+        && !turned_on.contains(elephant_current_valve)
+    {
+        let mut new_turned_on = turned_on.clone();
+        new_turned_on.insert(elephant_current_valve.clone());
+        elephant_configurations.push((
+            elephant_current_valve.clone(),
+            available_minutes as u32 * elephant_flow_rate,
+            new_turned_on,
+        ));
+    }
+    for next_valve in elephant_next_valves {
+        elephant_configurations.push((next_valve.clone(), 0, turned_on.clone()));
+    }
+
+    let mut result = vec![];
+    for my_conf in &my_configurations {
+        for elephant_conf in &elephant_configurations {
+            result.push((my_conf.clone(), elephant_conf.clone()));
+        }
+    }
+    result
+}
+
 fn solve_part_1(file_path: &str) -> u32 {
     let data = load_file(file_path);
     let valves_with_flow_rate_and_leads_to = parse_data(data);
@@ -105,14 +223,36 @@ fn solve_part_1(file_path: &str) -> u32 {
     )
 }
 
+fn solve_part_2(file_path: &str) -> u32 {
+    let data = load_file(file_path);
+    let valves_with_flow_rate_and_leads_to = parse_data(data);
+    let available_minutes = 26;
+    let start_valve = "AA";
+    solve_with_elephant(
+        &valves_with_flow_rate_and_leads_to,
+        &start_valve.to_string(),
+        &start_valve.to_string(),
+        available_minutes,
+        0,
+        HashSet::new(),
+        &mut HashMap::new(),
+    )
+}
+
 fn part_1(file_path: &str) {
     let result = solve_part_1(file_path);
     println!("Part 1 result: {:?}", result);
 }
 
+fn part_2(file_path: &str) {
+    let result = solve_part_2(file_path);
+    println!("Part 2 result: {:?} (too low 2435, to high 2980)", result);
+}
+
 fn main() {
     const FILE_PATH: &str = "./resources/puzzle.txt";
     part_1(FILE_PATH);
+    part_2(FILE_PATH);
 }
 
 #[cfg(test)]
@@ -123,5 +263,11 @@ mod tests {
     fn test_part_1() {
         let result = solve_part_1("./resources/test_data.txt");
         assert_eq!(result, 1651);
+    }
+
+    #[test]
+    fn test_part_2() {
+        let result = solve_part_2("./resources/test_data.txt");
+        assert_eq!(result, 1707);
     }
 }
